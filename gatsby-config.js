e5843dc2e7355787bd2ca9f561a3a86f79f79bb9
tylodung <39002385+tylodung@users.noreply.github.com>
2018-12-10 23:59:10 +0700

    up

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,12 +107,5 @@ module.exports = {
     },
   ],
 }
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129777503-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-129777503-1');
-</script>
+
